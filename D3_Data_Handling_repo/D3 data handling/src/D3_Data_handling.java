84fec7e34b5ba16c0c4d0aa72c224dbd86f3d184
Alcapone <lefebvre.ph2@gmail.com>
2013-07-31 08:10:15 +0200

    V0 to test git

--- D3_Data_Handling_repo/D3 data handling/src/D3_Data_handling.java
+++ D3_Data_Handling_repo/D3 data handling/src/D3_Data_handling.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.util.Arrays;
 
 public class D3_Data_handling {
 
@@ -8,9 +10,24 @@ public class D3_Data_handling {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println("file opened :" + args);
+		
+		BufferedReader CSVFile = 
+		        new BufferedReader(new FileReader(args[0]));
+		String dataRow = CSVFile.readLine(); 
+		while (dataRow != null){
+			   String[] dataArray = dataRow.split(",");
+			   for (String item:dataArray) { 
+			      System.out.print(item + "\t"); 
+			   }
+			   System.out.println(); // Print the data line.
+			   dataRow = CSVFile.readLine(); // Read next line of data.
+			  }
+		CSVFile.close();
+
+		
 
 	}
 
